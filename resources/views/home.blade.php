2e93701febff95087b5b85c0553f020a90648aa2
Mohammad Almllah <mohamedalmllah@gmail.com>
2020-11-12 22:17:18 +0200

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,30 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">عروض التقسيط الخاصة بي</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="text-right mb-3">
+                        <a class="btn btn-outline-primary" href="{{ route('installments.create') }}">
+                            عرض تقسيط جديد
+                        </a>
+                    </div>
+                    <div>
+                        @foreach ($installments as $installment)
+                        <div class="card mb-3">
+                            <div class="card-header text-center">
+                                <h1> {{$installment->product_name}} </h1>
+                            </div>
 
-                    {{ __('You are logged in!') }}
+                            <div class="card-body text-right">
+                                <h3> سعر المنتج : {{ $installment-> product_price }} جنيه </h3>
+                                <h3> مقدم التقسيط : {{ $installment-> advance_payment }} جنيه </h3>
+                                <h3> قسط {{ $installment-> installment_partition }} : {{ $installment-> installment_value }} جنيه </h3>
+                                <h3> عدد الاقساط : {{ ($installment->product_price - $installment->advance_payment) / $installment->installment_value }} </h3>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
